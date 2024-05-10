5e0c08be9befd524b4fa6ca68d86be81427de0ce
zhoufengen <zb-zhoufengen@kingmed.com.cn>
2024-05-10 17:55:58 +0800

    word spelling mistake

--- python_basics02/pratice02.py
+++ python_basics02/pratice02.py
@@ -3,8 +3,8 @@
 print("小明的成绩从去年的72分提升到了今年的85分，请计算小明成绩提升的百分点，并用字符串格式化显示出'xx.x%'，只保留小数点后1位")
 
 lastYearScore = 72
-thisYeasrSocre = 85
-delta = thisYeasrSocre - lastYearScore
+thisYearSocre = 85
+delta = thisYearSocre - lastYearScore
 myPercent = (delta / lastYearScore ) * 100
 
 print(f"小明成绩提升的百分点:{myPercent:.1f}")
